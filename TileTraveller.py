815f0d64e116dec5b3a32712a05046d128740c6a
Haflidi Haflidason <haflidih20@ru.is>
2020-09-17 09:36:03 +0000

    Tiletraveller algorithmi

---
+++ TileTraveller.py
@@ -0,0 +1,6 @@
+# Við ætlum fyrst að gera 4 föll, 1 fyrir hverja átt
+# Gerum fall sem gefur upp hvaða áttir eru í boði 
+# Gerum svo annað fall fyrir meldingu þegar farið er í átt sem er ekki leyfð
+# Gerum fall fyrir hreyfingu á milli gólfflísa (for loop)
+# Gerum fall sem að gefur meldingu um að leikurinn hafi klárast (victory)
+# Föllin eiga við hnitakerfi (x,y)
